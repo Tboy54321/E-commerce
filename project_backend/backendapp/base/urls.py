dbfe79779472813ea16e4cc48fb449c964ed0e14
tboy54321 <akinniranoluwatosin22@gmail.com>
2024-09-12 23:04:02 +0100

    Updating Templates

--- project_backend/backendapp/base/urls.py
+++ project_backend/backendapp/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="Home"),
-    path('search/', views.search_results, name="Search Results")
-    # path("room/", views.another, name="Room")
+    path('search/', views.search_results, name="Search Results"),
+    path('signin/', views.userlogin, name='Userlogin'),
+    path('signup/', views.usereg, name='Usereg')
 ]
--- project_backend/backendapp/base/views.py
+++ project_backend/backendapp/base/views.py
@@ -3,9 +3,20 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+context = [
+    {"id": "1", "title": "Specializing in luxury vehicles."},
+    {"id": "2", "title": "Expert car repair services."},
+    {"id": "3", "title": "Trusted vehicle dealerships."}
+]
 
 def home(request):
-    return render (request, 'homepage.html')
+    return render (request, 'homepage.html', {"context": context})
 
 def search_results(request):
     return render (request, 'searchresults.html')
+
+def userlogin(request):
+    return render (request, 'userlogin.html')
+
+def usereg(request):
+    return render (request, 'useregistration.html')
